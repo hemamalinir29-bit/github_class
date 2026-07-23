a5f7225d33e0f532ec1f4a07e62227e54ba938ed
Hema <your-email@example.com>
2026-07-23 11:27:05 +0530

    Message

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import Counter from "./Counter";
 import { Add, Subtract, Multiply, Divide } from "./Math";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./Navbar"
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+
       <Routes>
         <Route path="/counter" element={<Counter />} />
-        <Route path="/add " element={<Add />} />
-        <Route path="/Subtract " element={<Subtract />} />
-        <Route path="/Multiply" element={<Multiply />} />
-        <Route path="/Divide" element={<Divide />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/subtract" element={<Subtract />} />
+        <Route path="/multiply" element={<Multiply />} />
+        <Route path="/divide" element={<Divide />} />
       </Routes>
     </>
   );
 }
-export {Add,Subtract,Multiply,Divide}
+
 export default App;
